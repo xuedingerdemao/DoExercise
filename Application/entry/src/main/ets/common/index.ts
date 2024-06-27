8aa3f7d5a1b5e341bb78294bba41a3e092f63457
wb.chenzhize <wb.chenzhize@mesg.corp.netease.com>
2024-06-27 10:38:08 +0800

    first commit

---
+++ Application/entry/src/main/ets/common/index.ts
@@ -0,0 +1,85 @@
+export class Constants {
+  // 路由path
+  static readonly PAGE_INDEX = 'pages/Index'
+  static readonly PAGE_LOGIN = 'pages/Account/Login'
+  static readonly PAGE_HOME = 'pages/Home/Home'
+  static readonly PAGE_USER_CENTER = "pages/Account/UserCenter"
+  static readonly PAGE_ANSWER_DETAIL = "pages/Answer/AnswerDetail"
+  static readonly PAGE_ANSWER_RESULT = "pages/Answer/UserAnswerResult"
+  static readonly PAGE_CLASSIFY_LIST = "pages/Classify/ClassifyList"
+  static readonly PAGE_PAPER_LIST = "pages/Classify/PaperList"
+
+
+  // 固定的key
+  static readonly USER_STORE_KEY = "user_store_key" // 用户首选项的key
+  static readonly USER_AUTH_KEY = "user_auth_key" // 用户首选项的key
+  static readonly USER_TOP_KEY = "user_top_key" // 用户前三名的数据
+  static readonly USER_FORMID_KEY = "user_formId_key" // 用户前三名的数据
+
+
+
+  // 百分比
+  static readonly PERCENT_25 = '25%';
+  static readonly PERCENT_50 = '50%';
+  static readonly PERCENT_70 = '70%';
+  static readonly PERCENT_75 = '75%';
+  static readonly PERCENT_90 = '90%';
+  static readonly PERCENT_96 = '96%';
+  static readonly PERCENT_100 = '100%';
+
+  // 视口
+  static readonly VIEWPORT_50 = '50vp';
+
+
+  static readonly LENGTH_1_PX = 1;
+  static readonly LENGTH_2_PX = 2;
+  static readonly LENGTH_3_PX = 3;
+  static readonly LENGTH_5_PX = 5;
+  static readonly LENGTH_6_PX = 6;
+  static readonly LENGTH_8_PX = 8;
+  static readonly LENGTH_10_PX = 10;
+  static readonly LENGTH_15_PX = 15;
+  static readonly LENGTH_20_PX = 20;
+  static readonly LENGTH_30_PX = 30;
+  static readonly LENGTH_40_PX = 40;
+  static readonly LENGTH_70_PX = 70;
+  static readonly LENGTH_250_PX = 250;
+  static readonly LENGTH_100_PX = 100;
+  static readonly LENGTH_150_PX = 150;
+  static readonly LENGTH_160_PX = 160;
+  static readonly LENGTH_180_PX = 180;
+
+  static readonly BORDER_RADIUS_4_PX = 4;
+
+  static readonly HEIGHT_50VP = '50vp';
+  static readonly WIDTH_25_P = '25%';
+  static readonly WIDTH_50 = '50%';
+  static readonly WIDTH_70_P = '70%';
+  static readonly WIDTH_75_P = '75%';
+  static readonly WIDTH_90 = '90%';
+  static readonly FULL_WIDTH = '100%';
+  static readonly WIDTH_20 = 20;
+  static readonly WIDTH_70 = 70;
+  static readonly WIDTH_250 = 250;
+  static readonly HEIGHT_100 = 100;
+  static readonly HEIGHT_150 = 150;
+  static readonly HEIGHT_160 = 160;
+  static readonly HEIGHT_180 = 180;
+  static readonly HEIGHT_40 = 40;
+  static readonly HEIGHT_24 = 24;
+  static readonly HEIGHT_90 = '90%';
+  static readonly FULL_HEIGHT = '100%';
+  static readonly HEIGHT_50 = '50%';
+  static readonly MP_5 = 5;
+  static readonly MP_10 = 10;
+  static readonly MP_15 = 15;
+  static readonly MP_20 = 20;
+  static readonly MP_30 = 30;
+  static readonly BORDER_RADIUS = 4;
+  static readonly BORDER_WIDTH = 1;
+  static readonly WEIGHT_1 = 1;
+  static readonly WEIGHT_2 = 2;
+  static readonly WEIGHT_3 = 3;
+  static readonly MAX_LENGTH_6 = 6;
+  static readonly CHECKBOX_WIDTH = 8;
+}
